066dba97183f0f3bbd3474abccec07e68f1bbcb5
Bellar <gbellar@bethelks.edu>
2023-02-27 12:05:19 -0600

    Add save image functionality

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 import tkinter.filedialog
-from PIL import ImageTk, Image, ImageDraw, ImageFont
-# TODO: 1. Add ability to download watermarked images.
-#       2. Fix sizing and display issues with image. Needs to be centered. Same with buttons.
-#       3. Refactor functionality into classes. Main.py is confusing
-#       4. Error handling for clicking 'cancel' on upload box
+from PIL import ImageTk, Image
+# TODO: 1. Refactor functionality into classes. Main.py is confusing
+#       2. Error handling for clicking 'cancel' on upload box
+#       3. file save imrovements, default and allowed filetypes
+
+global image_to_save
 
 
 def resize_img_for_display(img: Image):
@@ -14,19 +15,22 @@ def resize_img_for_display(img: Image):
 
 
 def update_img_display():
+    global image_to_save
+
     fp = tkinter.filedialog.askopenfilename(parent=frame, title="Choose your image to watermark")
     image = Image.open(fp)
     width = image.width
     height = image.height
 
-    x_coord = int(width * 0.5)
-    y_coord = int(height * 0.5)
+    x_coord = int(width * 0.8)  # aiming for bottom right corner of image
+    y_coord = int(height * 0.8)
 
     watermark = Image.open("./upload/watermark.jpg").copy()
     watermark.thumbnail((600, 600))
 
     result = image.copy()
     result.paste(watermark, (x_coord, y_coord))
+    image_to_save = result
 
     resized_new_img = resize_img_for_display(result)
     # create tk photo image for passing to Label
@@ -34,6 +38,11 @@ def update_img_display():
     # update the label with new image
     img_display.configure(image=photo_img)
     img_display.image = photo_img  # for some reason I have to keep a reference to the new image
+
+
+def save_image():
+    file_path = tkinter.filedialog.asksaveasfile(initialfile="watermarked_image.jpg")
+    image_to_save.save(file_path)
 
 
 win = tk.Tk()
@@ -53,11 +62,12 @@ img_display = tk.Label(frame, image=photo_image)
 img_display.pack()
 
 
-upload_btn = tk.Button(frame, text="Upload", height=1, width=15, command=update_img_display)
+upload_btn = tk.Button(win, text="Upload", height=1, width=15, command=update_img_display)
 upload_btn.pack()
 
-# watermark_btn = tk.Button(frame, text="Add watermark", height=1, width=15)
-# watermark_btn.grid(row=2, column=0)
+download_btn = tk.Button(win, text="Download image", height=1, width=30, command=save_image)
+download_btn.pack()
+
 
 # Start the event loop.
 win.mainloop()
